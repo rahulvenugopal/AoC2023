c1a6ba4492367e477bce85ee91c1adff5d9c5faf
Rahul Venugopal <rhlvenugopal@gmail.com>
2023-12-24 13:20:32 +0530

    Day 3

---
+++ D3_Gear Ratios/D3.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  1 18:49:38 2023
+Day 1: Trebuchet
+
+@author: Rahul Venugopal
+"""
+#%% Part One
+# Load the data input
+# Load data which is in text format
+file = open('input.txt','r')
+data = file.readlines()
+data = [line.rstrip() for line in data]
+
+import re
+
+
+
+#%% Part Two 
